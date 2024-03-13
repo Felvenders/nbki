f7d2b57ec338e5b8e7526d35d45ec0b4b8393fae
Danila Potapkin <danila.potapkin@mirapolis.ru>
2024-03-13 21:17:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+/**
+ * TODO Class Description
+ *
+ * @author Danila Potapkin
+ * @since 13.03.2024
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
